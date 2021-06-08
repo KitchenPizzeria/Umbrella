971a9ea58c1061c1f9d9e22598caebb2f20116cb
KitchenPizzeria <70120980+KitchenPizzeria@users.noreply.github.com>
2021-06-08 12:52:20 +0100

    update

---
+++ Umbrella/ViewModel/SearchBoxViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBoxViewModel.swift
+//  Umbrella
+//
+//  Created by Joseph Meyrick on 08/06/2021.
+//
+
+import SwiftUI
+
+struct SearchBoxViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBoxViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBoxViewModel()
+    }
+}
---
+++ Umbrella/ViewModel/SnapshotViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  SnapshotViewModel.swift
+//  Umbrella
+//
+//  Created by Joseph Meyrick on 08/06/2021.
+//
+
+import SwiftUI
+
+struct SnapshotViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SnapshotViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        SnapshotViewModel()
+    }
+}
